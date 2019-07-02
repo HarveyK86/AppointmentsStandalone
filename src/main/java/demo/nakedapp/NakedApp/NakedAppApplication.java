ac380ec1e728993ae314003b9ffe6a0c3217b388
EC2 Default User <ec2-user@ip-172-31-25-196.eu-west-1.compute.internal>
2019-07-02 12:26:21 +0000

    Added base NakedApp from Spring Initializr

---
+++ src/main/java/demo/nakedapp/NakedApp/NakedAppApplication.java
@@ -0,0 +1,13 @@
+package demo.nakedapp.NakedApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NakedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NakedAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/nakedapp/NakedApp/NakedAppApplicationTests.java
@@ -0,0 +1,16 @@
+package demo.nakedapp.NakedApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NakedAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
